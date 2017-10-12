3a31fb4f6f763a9092412f38f7b1c2f116708358
John Gamboa <vaulttech@gmail.com>
2017-10-12 12:58:00 +0200

    Starts adapting the keras language model into pyTorch

--- main.py
+++ main.py
@@ -6,90 +6,148 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 from torchvision import datasets, transforms
+import torch.nn.functional as F
+from time import strftime, gmtime, time
 
-from model import VAE
-
-
-
-parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-parser.add_argument('--batch-size', type=int, default=128, metavar='N',
-                    help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='enables CUDA training')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging training status')
-args = parser.parse_args()
-args.cuda = not args.no_cuda and torch.cuda.is_available()
+import spacy
 
 
-torch.manual_seed(args.seed)
-if args.cuda:
-    torch.cuda.manual_seed(args.seed)
+import os
+import sys
+import pickle
+import numpy as np
 
 
-kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=True, download=True,
-                   transform=transforms.ToTensor()),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
+from model import LanguageModel
+
+def log(x):
+    print(x)
+
+def get_time(self):
+    return strftime('%Y-%m-%d %H:%M:%S', gmtime())
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch_size', type=int, default=128, metavar='N',
+                        help='input batch size for training (default: 128)')
+    parser.add_argument('--nb_epoch', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='enables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--language', type=str, default='en',
+                        help='Language code to be used. Default: "en";'
+                             ' Accepted: "en", "de".')
+    parser.add_argument('--validation_split', type=float, default=1,
+                        help='how much of the training set should be used for '
+                             'validation')
+    args = parser.parse_args()
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
+    return args
+
+
+def main(args):
+    # Initializes the random seed
+    torch.manual_seed(args.seed)
+    if args.cuda:
+        torch.cuda.manual_seed(args.seed)
+
+    # "Builds" the model and sends it to the GPU
+    model = LanguageModel()
+    if args.cuda:
+        model.cuda()
+
+    # Initializes spaCy
+    nlp = spacy.load(args.language)
+
+    # Defines a new optimizer
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+    # Run
+    train(model, optimizer, args, nlp)
+    test(model, optimizer, args, nlp)
+
+def load_dataset(self, which_dataset='train', load_column_vecs=False):
+    file_name = 'word_synonym_antonym.' + which_dataset
+    file_path = os.path.join('data', self.conf['dataset_name'], file_name)
+    triples = pickle.load(open(file_path, 'rb'))
+
+    words_vec    = torch.Tensor([self.nlp.vocab[i[0]].vector for i in triples])
+    synonyms_vec = torch.Tensor([self.nlp.vocab[i[1]].vector for i in triples])
+    antonyms_vec = torch.Tensor([self.nlp.vocab[i[2]].vector for i in triples])
+
+    if load_column_vecs:
+        words_vec    = words_vec.unqueeze(1)
+        synonyms_vec = synonyms_vec.unqueeze(1)
+        antonyms_vec = antonyms_vec.unqueeze(1)
+
+    words    = torch.from_numpy(np.array([i[0] for i in triples]))
+    synonyms = torch.from_numpy(np.array([i[1] for i in triples]))
+    antonyms = torch.from_numpy(np.array([i[2] for i in triples]))
+
+    return words_vec, synonyms_vec, antonyms_vec, words, synonyms, antonyms
+
+
+def loss_function(x, distance, args):
+    # First, separate the input
+    target_word, synonym, antonym = x
+
+    # Calculate the distance between all elements
+    good_distance = distance(target_word, synonym)
+    bad_distance = distance(target_word, antonym)
+
+    loss = F.relu(args.margin - good_distance + bad_distance)
+    return loss
+
+
+def train(model, optimizer, nlp, args):
+    batch_size = args.batch_size
+    nb_epoch = args.nb_epoch
+    validation_split = args.validation_split
+
+    words_vec, synonyms_vec, antonyms_vec, words, synonyms, antonyms = \
+                                                        load_dataset()
+    assert len(words_vec) == len(synonyms_vec) == len(antonyms_vec)
+
+    log('Began training at %s on %d samples' % (get_time(), len(words_vec)))
+
+    val_loss = {'loss': 1., 'epoch': 0}
+
+    good = nlp.vocab['good'].vector[np.newaxis, :]
+    bad = nlp.vocab['bad'].vector[np.newaxis, :]
+
+    # Maybe it is best not to care about epochs
+    for i in range(1, nb_epoch+1):
+        print('Fitting epoch %d' % i, file=sys.stderr)
 
 
 
-model = VAE()
-if args.cuda:
-    model.cuda()
+        # hist = self.model.fit([words_vec, synonyms_vec, antonyms_vec], nb_epoch=1,
+        #                   batch_size=batch_size,
+        #                   validation_split=validation_split, verbose=1)
 
-reconstruction_function = nn.BCELoss()
-reconstruction_function.size_average = False
-
-
-def loss_function(recon_x, x, mu, logvar):
-    BCE = reconstruction_function(recon_x, x.view(-1, 784))
-
-    # see Appendix B from VAE paper:
-    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-    # https://arxiv.org/abs/1312.6114
-    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD_element = mu.pow(2).add_(logvar.exp()).mul_(-1).add_(1).add_(logvar)
-    KLD = torch.sum(KLD_element).mul_(-0.5)
-
-    return BCE + KLD
-
-
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
-
-
-def train(epoch):
-    model.train()
-    train_loss = 0
-    for batch_idx, (data, _) in enumerate(train_loader):
         data = Variable(data)
         if args.cuda:
             data = data.cuda()
-        optimizer.zero_grad()
-        recon_batch, mu, logvar = model(data)
-        loss = loss_function(recon_batch, data, mu, logvar)
-        loss.backward()
-        train_loss += loss.data[0]
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader),
-                loss.data[0] / len(data)))
+        x = model(data)
+        # loss = loss_function(x, distance, args)
+        # loss.backward()
+        # train_loss += loss.data[0]
+        # optimizer.step()
+        # if batch_idx % args.log_interval == 0:
+        #     print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+        #         epoch, batch_idx * len(data), len(train_loader.dataset),
+        #         100. * batch_idx / len(train_loader),
+        #         loss.data[0] / len(data)))
+        #
+        # This should happen by the end of an epoch
+        # print('====> Epoch: {} Average loss: {:.4f}'.format(
+        #     epoch, train_loss / len(train_loader.dataset)))
 
-    print('====> Epoch: {} Average loss: {:.4f}'.format(
-          epoch, train_loss / len(train_loader.dataset)))
+    return val_loss
 
-
-def test(epoch):
+def test(model, optimizer, nlp, args):
     model.eval()
     test_loss = 0
     for data, _ in test_loader:
@@ -101,8 +159,3 @@ def test(epoch):
 
     test_loss /= len(test_loader.dataset)
     print('====> Test set loss: {:.4f}'.format(test_loss))
-
-
-for epoch in range(1, args.epochs + 1):
-    train(epoch)
-    test(epoch)
