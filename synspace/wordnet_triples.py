dacd7162ec9d5ecc91163701011a53a2de0a5cd0
John Gamboa <vaulttech@gmail.com>
2017-10-13 10:28:25 +0200

    Removes that dictionary: dataloader now gives triples

--- synspace/wordnet_triples.py
+++ synspace/wordnet_triples.py
@@ -12,9 +12,12 @@ class ToTensor():
     """Convert tuples in sample to Tensors."""
 
     def __call__(self, sample):
-        return {'target_word': torch.Tensor([sample['target_word']]),
-                'synonym': torch.Tensor([sample['synonym']]),
-                'antonym': torch.Tensor([sample['antonym']]) }
+        # return {'target_word': torch.Tensor([sample['target_word']]),
+        #         'synonym': torch.Tensor([sample['synonym']]),
+        #         'antonym': torch.Tensor([sample['antonym']]) }
+        return (torch.Tensor([sample[0]]),
+                torch.Tensor([sample[1]]),
+                torch.Tensor([sample[2]]))
 
 
 class WordnetTriples(Dataset):
@@ -34,9 +37,10 @@ class WordnetTriples(Dataset):
 
         # Transform the triple into word indexes. Now it looks like
         #    (101, 324, 671)
-        sample = {'target_word': self.w2i[sample[0]],
-                  'synonym': self.w2i[sample[1]],
-                  'antonym': self.w2i[sample[2]] }
+        # sample = {'target_word': self.w2i[sample[0]],
+        #           'synonym': self.w2i[sample[1]],
+        #           'antonym': self.w2i[sample[2]] }
+        sample = (self.w2i[sample[0]], self.w2i[sample[1]], self.w2i[sample[2]])
 
         # Maybe transform further the value
         if self.transform:
@@ -45,7 +49,7 @@ class WordnetTriples(Dataset):
         return sample
 
 def get_triples_loader(input_file, vocab_path,
-                       batch_size, shuffle, num_workers):
+                       batch_size, shuffle, num_workers=4):
     raw_triples = pickle.load(open(input_file, "rb"))
 
     train_triples, val_triples = split_triples(raw_triples, 0.05)
@@ -71,12 +75,15 @@ def parse_args():
 
 
 def main(args):
-    dataset = WordnetTriples(args.input_file, args.vocab_path, ToTensor())
-    dataloader = DataLoader(dataset, batch_size=4, shuffle=True, num_workers=4)
+    train_dl, val_dl = get_triples_loader(args.input_file, args.vocab_path,
+                                            batch_size=4, shuffle=True)
 
-    for i_batch, sample_batched in enumerate(dataloader):
-        print(i_batch, sample_batched['target_word'].size(),
-              sample_batched['synonym'].size(), sample_batched['antonym'].size())
+    for i_batch, sample_batched in enumerate(train_dl):
+        # print(i_batch, sample_batched['target_word'].size(),
+        #       sample_batched['synonym'].size(), sample_batched['antonym'].size())
+
+        print(i_batch, sample_batched[0].size(),
+              sample_batched[1].size(), sample_batched[2].size())
 
     # for i in range(10):
     #     s = dataset[i]
